f9027fd23a3e03c889735d06b1155a630fb9a88d
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2021-12-21 15:32:25 +0100

    asked age e kilometer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log('JS OK!')
+console.log('JS OK!');
+
+/* Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.*/
+
+
+// 1- Chiedere all'utente quanti chilometri dovrà percorrere
+// 2- Chiedere l'età del passeggero
+// 3- Calcolare il prezzo in base a quanti chilometri percorrerà
+// 4- Applicare sconto del 20% per i minorenni
+// 5- Applicare sconto del 40% per gli over 65
+// 6- Prezzo finale in forma umana (massimo due decimali)
+
+const kilometer = parseInt(prompt ('Quanti km dovrai percorrere?', '20').trim());
+const userAge = parseInt(prompt ('Quanti anni hai?', '30').trim());
+
+console.log('kilometer: ', kilometer);
+console.log('userAge: ', userAge);
