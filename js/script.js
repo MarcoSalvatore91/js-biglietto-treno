a9482d2d5f3c606e52fb96e13ec001b42d1ea6bc
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2021-12-21 15:48:38 +0100

    added alert about info_user

--- js/script.js
+++ js/script.js
@@ -10,13 +10,24 @@ L'output del prezzo finale va messo fuori in forma umana (con massimo due decima
 
 // 1- Chiedere all'utente quanti chilometri dovrà percorrere
 // 2- Chiedere l'età del passeggero
-// 3- Calcolare il prezzo in base a quanti chilometri percorrerà
-// 4- Applicare sconto del 20% per i minorenni
-// 5- Applicare sconto del 40% per gli over 65
-// 6- Prezzo finale in forma umana (massimo due decimali)
+// 3- Controllare che siano inseriti solamente caratteri numerici
+// 4- Calcolare il prezzo in base a quanti chilometri percorrerà
+// 5- Applicare sconto del 20% per i minorenni
+// 6- Applicare sconto del 40% per gli over 65
+// 7- Prezzo finale in forma umana (massimo due decimali)
 
+// 1- Chiedere all'utente quanti chilometri dovrà percorrere
 const kilometer = parseInt(prompt ('Quanti km dovrai percorrere?', '20').trim());
+// 2- Chiedere l'età del passeggero
 const userAge = parseInt(prompt ('Quanti anni hai?', '30').trim());
 
 console.log('kilometer: ', kilometer);
 console.log('userAge: ', userAge);
+
+// 3- Controllare che siano inseriti solamente caratteri numerici
+//! Compilare correttamente i campi richiesti
+if (isNaN(kilometer) || isNaN(userAge)) {
+    alert("Attenzione! E' permesso inserire solamente caratteri numerici!");
+}
+
+// 4- Calcolare il prezzo in base a quanti chilometri percorrerà
